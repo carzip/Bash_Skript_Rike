271a99ef1d615313279164b79530e6633f5fc7ab
Rike Carpantier <Rike.Carpantier@ruhr-uni-bochum.de>
2022-11-25 13:41:46 +0100

    commit Message

---
+++ count_lines.sh
@@ -0,0 +1,7 @@
+# count_lines.sh
+# Jahr, mein Name
+# Count thelines of all TCV files
+# in the current folder
+echo "Count all lines of TSV files"
+wc -l *tsv
+echo "Counting done - have a nice day"
