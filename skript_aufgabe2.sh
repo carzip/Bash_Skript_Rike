183c9e732c1170716d6c2c58b2b1a0f177af431d
carzip <70385255+carzip@users.noreply.github.com>
2022-12-19 12:54:11 +0100

    Shorten script (use >> instead of cat two files)

--- skript_aufgabe2.sh
+++ skript_aufgabe2.sh
@@ -7,9 +7,8 @@
 
 echo "Extract ISSN and year"
 # due to "slipped" cells in individual steps (is probably more cumbersome than using sed -r 's/IMPORTANT\t\t/' to cut out the extra cells, but is currently clearer.)
-grep -v IMPORTANT 2022-11-25-Article_list_dirty.tsv | cut -f 5,12 | sed '1d' > erg_part1.tsv
-grep IMPORTANT 2022-11-25-Article_list_dirty.tsv | cut -f 7,14 > erg_part2.tsv
-cat erg_part1.tsv erg_part2.tsv > erg_dirty.tsv
+grep -v IMPORTANT 2022-11-25-Article_list_dirty.tsv | cut -f 5,12 | sed '1d' > erg_dirty.tsv
+grep IMPORTANT 2022-11-25-Article_list_dirty.tsv | cut -f 7,14 >> erg_dirty.tsv
 sed 's/issn//I;s/://;s/ //g' erg_dirty.tsv | sort | uniq | sed '1,2d' > 2022-11-25-Dates_and_ISSNs_RC.tsv
 echo "Results saved as 2022-11-25-Dates_and_ISSNs_RC.tsv"
 
